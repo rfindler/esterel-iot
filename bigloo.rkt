e5811d1236225352cbb09cc9ea449a04dce5f5ed
Robby Findler <robby@racket-lang.org>
2024-05-06 13:17:15 -0500

    add more support to bigloo.rkt

--- bigloo.rkt
+++ bigloo.rkt
@@ -1,11 +1,15 @@
 #lang racket
 (require (for-syntax syntax/parse syntax/readerr)
-         racket/fixnum racket/hash)
+         racket/fixnum)
+(module+ test (require rackunit))
 
 (provide (rename-out [b:assert assert]
                      [b:define define]
                      [b:module module]
                      [b:define-inline define-inline]
+                     [b:open-input-string open-input-string]
+                     [b:call-with-input-string call-with-input-string]
+                     [b:string=? string=?]
                      [read-string read-chars])
          multiple-value-bind
          include/bigloo
@@ -13,11 +17,15 @@
          with-trace
          trace-item
          tprint
-         unwind-protect)
+         unwind-protect
+         reverse!
+         pregexp
+         string-index)
 
 ;; renamings
 (provide (rename-out [fx* *fx]
                      [fx+ +fx]
+                     [fx- -fx]
                      [fx= =fx]
                      [fx> >fx]
                      [fx>= >=fx]
@@ -26,10 +34,13 @@
                      [bitwise-ior bit-or]
                      [arithmetic-shift bit-lsh]
                      [flush-output flush-output-port]
-                     [display display-string]))
+                     [display display-string]
+                     [b:pregexp-match pregexp-match]
+                     [string-index string-char-index]
+                     [list* cons*]
+                     ))
 
-(struct bigloo-object
-  (ht))
+(struct bigloo-object (ht))
 
 (define (bit-rsh n m)
   (arithmetic-shift n (- m)))
@@ -92,6 +103,16 @@
      #'(define (fname.id mandatory.id ...)
          body1 body2 ...)]))
 
+(define (extend-object ht k+vs)
+  (define the-result
+    (cond
+      [(not ht) (make-hash)]
+      [(bigloo-object? ht) (hash-copy (bigloo-object-ht ht))]
+      [else (error 'extend-object "internal error ~s" ht)]))
+  (for ([pr (in-list k+vs)])
+    (match-define (cons k v) pr)
+    (hash-set! the-result k v))
+  (bigloo-object the-result))
 
 (begin-for-syntax
   (define-syntax-class field-prop
@@ -154,28 +175,29 @@
                    (make-method-name "instantiate::" #'name))
              #:attr (definitions 1)
              (list #`(define #,(strip-type #'name)
-                       (hash-union! #,(or (extract-type #'name) (make-hash))
-                                    (make-hash (append f.init-expr ...))))
+                       (extend-object #,(or (extract-type #'name) #'#f)
+                                      (append f.init-expr ...)))
                    #`(define-syntax #,(make-method-name "with-access::" #'name)
                        (with-access-transformer #'name (list #'f ...)))
                    #`(define-syntax #,(make-method-name "instantiate::" #'name)
                        (lambda (stx)
                          (syntax-parse stx
                            [(_ (fname:id fexpr:expr) (... ...))
-                            #`(hash-union! #,(strip-type #'name)
-                                           (make-hash (list (cons 'fname fexpr) (... ...))))]))))))
+                            #`(extend-object #,(strip-type #'name)
+                                             (list (cons 'fname fexpr)
+                                                   (... ...)))]))))))
   (define-syntax-class export-clause
     #:attributes ((export 1) (code 0))
     #:datum-literals (inline)
     (pattern x:id
       #:attr code #'(void)
-      #:attr (export 1) (list #'x))
+      #:attr (export 1) (list (strip-type #'x)))
     (pattern (inline x:id arg:id ...)
       #:attr code #'(void)
-      #:attr (export 1) (list #'x))
+      #:attr (export 1) (list (strip-type #'x)))
     (pattern (x:id arg:id ...)
       #:attr code #'(void)
-      #:attr (export 1) (list #'x))
+      #:attr (export 1) (list (strip-type #'x)))
     (pattern cls:class
       #:attr code #'(b:define-class cls)
       #:attr (export 1) (syntax->list #'(cls.exports ...))))
@@ -195,6 +217,64 @@
      (trace-stx
       #`(begin cls.definitions ...))]))
 
+(module+ test
+  (b:define-class
+    (class class-name
+      (f1::byte read-only)
+      (f2 read-only)
+      (f-w-default (default -1))))
+  (check-equal? (bigloo-object-ht (instantiate::class-name (f1 1) (f2 2)))
+                (make-hash (list (cons 'f1 1)
+                                 (cons 'f2 2)
+                                 (cons 'f-w-default -1))))
+  (check-equal? (bigloo-object-ht (instantiate::class-name (f2 2) (f1 1)))
+                (make-hash (list (cons 'f1 1)
+                                 (cons 'f2 2)
+                                 (cons 'f-w-default -1))))
+  (check-equal? (bigloo-object-ht
+                 (instantiate::class-name (f2 2) (f1 1) (f-w-default 11)))
+                (make-hash (list (cons 'f1 1)
+                                 (cons 'f2 2)
+                                 (cons 'f-w-default 11))))
+  (check-equal? (bigloo-object-ht
+                 (instantiate::class-name (f-w-default 11) (f2 2) (f1 1)))
+                (make-hash (list (cons 'f1 1)
+                                 (cons 'f2 2)
+                                 (cons 'f-w-default 11))))
+
+  (b:define-class
+    (class sub-class-name::class-name
+      (f3 read-only)
+      (f-w-default2 (default -2))))
+  (check-equal? (bigloo-object-ht (instantiate::sub-class-name (f2 2) (f1 1) (f3 3)))
+                (make-hash (list (cons 'f1 1)
+                                 (cons 'f2 2)
+                                 (cons 'f3 3)
+                                 (cons 'f-w-default -1)
+                                 (cons 'f-w-default2 -2))))
+
+  (check-equal?
+   (with-access::class-name (instantiate::class-name (f2 2) (f1 1) (f-w-default 11))
+     (f2)
+     f2)
+   2)
+  (check-equal?
+   (with-access::class-name (instantiate::class-name (f2 2) (f1 1) (f-w-default 11))
+     (f1 f2 f-w-default)
+     (list f1 f2 f-w-default))
+   (list 1 2 11))
+  (check-equal?
+   (with-access::sub-class-name (instantiate::sub-class-name (f2 2) (f1 1) (f3 3))
+     (f3)
+     f3)
+   3)
+  (check-equal?
+   (with-access::sub-class-name (instantiate::sub-class-name (f2 2) (f1 1) (f3 3))
+     (f1 f2 f3 f-w-default f-w-default2)
+     (list f1 f2 f3 f-w-default f-w-default2))
+   (list 1 2 3 -1 -2))
+  )
+
 (define-syntax (b:module stx)
   (syntax-parse stx
     [(_ name:identifier clause:module-clause ...)
@@ -208,7 +288,7 @@
 (define-syntax (b:assert stx)
   (syntax-parse stx
     [(_ _ condition)
-     #'(unless condition (error "assert failed"))]))
+     #`(unless condition #,(syntax/loc stx (error "assert failed")))]))
 
 (define-syntax (with-trace stx)
   (syntax-parse stx
@@ -239,3 +319,47 @@
 
 (define-syntax-rule (multiple-value-bind (id ...) expr body ...)
   (call-with-values (lambda () expr) (lambda (id ...) body ...)))
+
+(define-syntax (reverse! stx)
+  (syntax-parse stx
+    [(_ arg)
+     (printf "suspicious: replacing reverse! with reverse ~a:~a\n" (syntax-source stx) (syntax-line stx))
+     #`(reverse arg)]))
+
+(define (b:pregexp-match reg str)
+  (unless (pregexp? reg)
+    (error 'b:pregexp-match "expected a pregexp"))
+  (regexp-match reg str))
+
+(define (string-index s ch)
+  (define reg
+    (cond
+      [(string? ch) (regexp (regexp-quote ch))]
+      [else (regexp (regexp-quote (string ch)))]))
+  (define p (regexp-match-positions reg s))
+  (and p (car (list-ref p 0))))
+
+(module+ test
+  (check-equal? (string-index "abc" #\a) 0)
+  (check-equal? (string-index "abc" #\b) 1)
+  (check-equal? (string-index "abc" "a") 0)
+  (check-equal? (string-index "abc" "b") 1)
+  (check-equal? (string-index "abbc" "bc") 2))
+
+(define (b:open-input-string str)
+  (cond
+    [(string? str) (open-input-string str)]
+    [(bytes? str) (open-input-bytes str)]
+    [else (error 'b:open-input-string "expected (or/c string? bytes?), got ~s" str)]))
+
+(define (b:call-with-input-string str proc)
+  (proc (b:open-input-string str)))
+
+(define (b:string=? x y)
+  (define (to-bytes n)
+    (cond
+      [(bytes? n) n]
+      [(string? n) (string->bytes/utf-8 n)]
+      [else (error 'b:string=? "bad args ~s ~s" x y)]))
+  (bytes=? (to-bytes x)
+           (to-bytes y)))
